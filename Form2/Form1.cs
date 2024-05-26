c8d914b67bef520061b3d76484fe9f4041d78644
seckingurlek <seckingurlek45@gmail.com>
2024-05-27 01:34:24 +0300

    last commit

--- Form2/Form1.cs
+++ Form2/Form1.cs
@@ -1,10 +1,9 @@
 using Microsoft.Data.SqlClient;
 using System.Data;
-using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Threading;
-using System.Transactions;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Form2
 {
@@ -15,13 +14,16 @@ namespace Form2
         private const string RepeatableRead = "RepeatableRead";
         private const string Serializable = "Serializable";
 
-        static readonly string connectionString = 
+        static readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AdventureWorks2019;Integrated Security=True;Trust Server Certificate=True";
         static readonly string detailTable = "Sales.SalesOrderDetail";
         static readonly string headerTable = "Sales.SalesOrderHeader";
+
+        private static int writerDeadlockCount = 0; // Shared writer deadlock counter
+        private static int readerDeadlockCount = 0; // Shared reader deadlock counter
+
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void start_btn_Click(object sender, EventArgs e)
@@ -29,57 +31,41 @@ namespace Form2
             string isolationLevel = this.isolationLevel.Text;
             int writerCount = (int)typeAUser.Value;
             int readerCount = (int)typeBUser.Value;
-            var isolationLevels = new (string, int, int)[]
-                    {
-                            ("READ UNCOMMITTED", 3, 5),
-                            ("READ COMMITTED", 5, 3),
-                            ("REPEATABLE READ", 4, 4),
-                            ("SERIALIZABLE", 2, 6),
-                            ("SNAPSHOT", 3, 3)
-                    };
-            string SelectedIsolationLevel = "";
-            switch (isolationLevel)
+
+            string selectedIsolationLevel = isolationLevel switch
             {
-                case ReadCommitted:
-                    SelectedIsolationLevel = "READ COMMITTED";
-                    break;
-                case ReadUncommitted:
-                    SelectedIsolationLevel = "READ UNCOMMITTED";
-                    break;
-                case RepeatableRead:
-                    SelectedIsolationLevel = "REPEATABLE READ";
-                    break;
-                case Serializable:
-                    SelectedIsolationLevel = "SERIALIZABLE";
-                    break;
+                ReadCommitted => "READ COMMITTED",
+                ReadUncommitted => "READ UNCOMMITTED",
+                RepeatableRead => "REPEATABLE READ",
+                Serializable => "SERIALIZABLE",
+                _ => string.Empty,
+            };
 
-
-                default:
-                    break;
+            if (string.IsNullOrEmpty(selectedIsolationLevel))
+            {
+                MessageBox.Show("Please select a valid isolation level.");
+                return;
             }
 
-                Console.WriteLine($"Starting simulation for {SelectedIsolationLevel} with {writerCount} writers and {readerCount} readers.");
-                RunSimulation(SelectedIsolationLevel, writerCount, readerCount);
-                Console.WriteLine($"Simulation completed for {SelectedIsolationLevel}.\n");
-          
-
+            Console.WriteLine($"Starting simulation for {selectedIsolationLevel} with {writerCount} writers and {readerCount} readers.");
+            RunSimulation(selectedIsolationLevel, writerCount, readerCount);
+            Console.WriteLine($"Simulation completed for {selectedIsolationLevel}.\nWriter deadlocks: {writerDeadlockCount}\nReader deadlocks: {readerDeadlockCount}");
         }
-
-
-
 
         static void RunSimulation(string isolationLevel, int writerCount, int readerCount)
         {
+            writerDeadlockCount = 0; // Reset writer deadlock counter
+            readerDeadlockCount = 0; // Reset reader deadlock counter
             Task[] tasks = new Task[writerCount + readerCount];
 
-            // Yazýcý thread'leri baþlat
+            // Start writer threads
             for (int i = 0; i < writerCount; i++)
             {
                 int threadId = i + 1;
                 tasks[i] = Task.Run(() => UpdateDatabase(isolationLevel, threadId));
             }
 
-            // Okuyucu thread'leri baþlat
+            // Start reader threads
             for (int i = writerCount; i < writerCount + readerCount; i++)
             {
                 int threadId = i + 1;
@@ -104,13 +90,14 @@ namespace Form2
                     command.ExecuteNonQuery();
 
                     var dateRanges = new List<(string, string)>
-                {
-                    ("20110101", "20111231"),
-                    ("20120101", "20121231"),
-                    ("20130101", "20131231"),
-                    ("20140101", "20141231"),
-                    ("20150101", "20151231")
-                };
+                    {
+                        ("20110101", "20111231"),
+                        ("20120101", "20121231"),
+                        ("20130101", "20131231"),
+                        ("20140101", "20141231"),
+                        ("20150101", "20151231")
+                    };
+
                     for (int i = 0; i < 100; i++)
                     {
                         try
@@ -120,36 +107,34 @@ namespace Form2
                                 if (rand.NextDouble() < 0.5)
                                 {
                                     command.CommandText = $@"
-                            UPDATE {detailTable}
-                            SET UnitPrice = UnitPrice * 10.0 / 10.0
-                            WHERE UnitPrice > 100
-                            AND EXISTS (
-                                SELECT * FROM {headerTable}
-                                WHERE {headerTable}.SalesOrderID = {detailTable}.SalesOrderID
-                                AND {headerTable}.OrderDate BETWEEN '{beginDate}' AND '{endDate}'
-                                AND {headerTable}.OnlineOrderFlag = 1
-                            );";
+                                        UPDATE {detailTable}
+                                        SET UnitPrice = UnitPrice * 10.0 / 10.0
+                                        WHERE UnitPrice > 100
+                                        AND EXISTS (
+                                            SELECT * FROM {headerTable}
+                                            WHERE {headerTable}.SalesOrderID = {detailTable}.SalesOrderID
+                                            AND {headerTable}.OrderDate BETWEEN '{beginDate}' AND '{endDate}'
+                                            AND {headerTable}.OnlineOrderFlag = 1
+                                        );";
                                     command.ExecuteNonQuery();
                                     Console.WriteLine($"Update thread {threadId} ({isolationLevel}): ");
-
                                 }
                             }
                         }
                         catch (SqlException ex)
                         {
-                            
                             if (ex.Number == 1205) // Deadlock error number in SQL Server
                             {
-                                Console.WriteLine($"Deadlock occurred in reader thread {threadId} On WRITER THREAD");
-                            } 
+                                Interlocked.Increment(ref writerDeadlockCount);
+                                Console.WriteLine($"Deadlock occurred in writer thread {threadId}");
+                            }
                         }
                     }
-                  
                 }
             }
 
             stopwatch.Stop();
-            Console.WriteLine($"Update thread {threadId} ({isolationLevel}) completed in {stopwatch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Update thread {threadId} ({isolationLevel}) completed in {stopwatch.Elapsed.TotalSeconds:F2} seconds.");
         }
 
         static void ReadDatabase(string isolationLevel, int threadId)
@@ -167,14 +152,15 @@ namespace Form2
                     command.ExecuteNonQuery();
 
                     var dateRanges = new List<(string, string)>
-                {
-                    ("20110101", "20111231"),
-                    ("20120101", "20121231"),
-                    ("20130101", "20131231"),
-                    ("20140101", "20141231"),
-                    ("20150101", "20151231")
-                };
-                    for (int i = 0;i<100;i++)
+                    {
+                        ("20110101", "20111231"),
+                        ("20120101", "20121231"),
+                        ("20130101", "20131231"),
+                        ("20140101", "20141231"),
+                        ("20150101", "20151231")
+                    };
+
+                    for (int i = 0; i < 100; i++)
                     {
                         try
                         {
@@ -183,15 +169,15 @@ namespace Form2
                                 if (rand.NextDouble() < 0.5)
                                 {
                                     command.CommandText = $@"
-                            SELECT SUM({detailTable}.OrderQty)
-                            FROM {detailTable}
-                            WHERE UnitPrice > 100
-                            AND EXISTS (
-                                SELECT * FROM {headerTable}
-                                WHERE {headerTable}.SalesOrderID = {detailTable}.SalesOrderID
-                                AND {headerTable}.OrderDate BETWEEN '{beginDate}' AND '{endDate}'
-                                AND {headerTable}.OnlineOrderFlag = 1
-                            );";
+                                        SELECT SUM({detailTable}.OrderQty)
+                                        FROM {detailTable}
+                                        WHERE UnitPrice > 100
+                                        AND EXISTS (
+                                            SELECT * FROM {headerTable}
+                                            WHERE {headerTable}.SalesOrderID = {detailTable}.SalesOrderID
+                                            AND {headerTable}.OrderDate BETWEEN '{beginDate}' AND '{endDate}'
+                                            AND {headerTable}.OnlineOrderFlag = 1
+                                        );";
                                     using (var reader = command.ExecuteReader())
                                     {
                                         while (reader.Read())
@@ -205,39 +191,26 @@ namespace Form2
                         }
                         catch (SqlException ex)
                         {
-                            
                             if (ex.Number == 1205) // Deadlock error number in SQL Server
                             {
-                                Console.WriteLine($"Deadlock occurred in reader thread {threadId} On READER THREAD");
+                                Interlocked.Increment(ref readerDeadlockCount);
+                                Console.WriteLine($"Deadlock occurred in reader thread {threadId}");
                             }
                         }
                     }
-                   
                 }
             }
 
             stopwatch.Stop();
-            Console.WriteLine($"Read thread {threadId} ({isolationLevel}) completed in {stopwatch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Read thread {threadId} ({isolationLevel}) completed in {stopwatch.Elapsed.TotalSeconds:F2} seconds.");
         }
-
-
 
         private void Form1_Load(object sender, EventArgs e) { }
 
+        private void typeBUser_ValueChanged(object sender, EventArgs e) { }
 
-        private void typeBUser_ValueChanged(object sender, EventArgs e)
-        {
+        private void typeAUser_ValueChanged(object sender, EventArgs e) { }
 
-        }
-
-        private void typeAUser_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void isolationLevel_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        private void isolationLevel_SelectedIndexChanged(object sender, EventArgs e) { }
     }
 }
